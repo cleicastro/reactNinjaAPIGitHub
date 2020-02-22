e41c0603f5ba23525a885d3c95a7d38bfb3f7066
cleicastro <cleicastro.ti@hotmail.com>
2020-02-22 15:05:18 -0300

    Preparando o test com jest

---
+++ src/componentes/Aula1.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class appTeste extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Teste</h1>
+            </div>
+        )
+    }
+}
+
+export default { appTeste }
+
