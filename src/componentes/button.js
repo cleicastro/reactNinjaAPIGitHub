6fc6268a58e16ed7bfd3896c556cafd8575f57e1
cleicastro <cleicastro.ti@hotmail.com>
2020-02-25 10:10:33 -0300

    remocao dos componentes para prepar o novo app

--- src/componentes/button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Button = (({ children, handleClick, name }) => (
-    <button type="button" className={name} onClick={handleClick}>{children}</button>
-))
-
-export default Button
